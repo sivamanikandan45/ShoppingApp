03dfec0a64d0950009bd7ffc4d99386c6fc3ee4d
siva-14489 <sivamanikandan45@gmail.com>
2022-10-10 18:42:25 +0530

    -

--- app/src/main/java/com/example/shopping/PaymentFragment.kt
+++ app/src/main/java/com/example/shopping/PaymentFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.findFragment
 
 class PaymentFragment : Fragment() {
 
@@ -21,6 +26,51 @@ class PaymentFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.title="Payment"
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val paymentOptionRadioGroup=view.findViewById<RadioGroup>(R.id.payment_options)
+        val placeOrderBtn=view.findViewById<Button>(R.id.place_order_btn)
+        val payButton=view.findViewById<Button>(R.id.pay)
+        paymentOptionRadioGroup.setOnCheckedChangeListener { radioGroup, checkedId ->
+            payButton.isEnabled=true
+            when(checkedId){
+                R.id.upi_payment->{
+                    placeOrderBtn.visibility=View.GONE
+                    payButton.visibility=View.VISIBLE
+                }
+                R.id.card_payment->{
+                    payButton.visibility=View.VISIBLE
+                    placeOrderBtn.visibility=View.GONE
+                }
+                R.id.cod->{
+                    payButton.visibility=View.GONE
+                    placeOrderBtn.visibility=View.VISIBLE
+                }
+            }
+        }
+
+        placeOrderBtn.setOnClickListener {
+            replaceFragment(OrderPlacedFragment())
+        }
+
+        payButton.setOnClickListener {
+            val upiBtn=view.findViewById<RadioButton>(R.id.upi_payment)
+            val cardBtn=view.findViewById<RadioButton>(R.id.card_payment)
+
+            when{
+                upiBtn.isChecked->{
+                    replaceFragment(UpiFragment())
+                }
+                cardBtn.isChecked->{
+                    replaceFragment(CardPaymentFragment())
+                }
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager.commit {
+            addToBackStack(null)
+            replace(R.id.checkout_fragment_container, fragment)
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/shopping/UpiFragment.kt
@@ -0,0 +1,93 @@
+package com.example.shopping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.google.android.material.textfield.TextInputLayout
+import java.lang.String
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+
+class UpiFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_upi, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val payBtn=view.findViewById<Button>(R.id.pay)
+        payBtn.setOnClickListener {
+            if(validateUPIId()){
+                parentFragmentManager.commit {
+                    addToBackStack(null)
+                    replace(R.id.checkout_fragment_container, OrderPlacedFragment())
+                }
+            }
+        }
+
+    }
+
+    private fun validateUPIId(): Boolean {
+        var returnValue=true
+        val p: Pattern = Pattern.compile("[a-zA-Z0-9.\\-_]{2,256}@[a-zA-Z]{2,64}") //. represents single character
+        val upiId=view?.findViewById<TextInputLayout>(R.id.upi_id)
+        val id=upiId?.editText?.text.toString()
+
+        val upiPin=view?.findViewById<TextInputLayout>(R.id.upi_pin)
+        val pin=upiPin?.editText?.text.toString()
+
+        val m: Matcher = p.matcher(id)
+
+        if(upiId!=null){
+            if(id==""){
+                upiId.isErrorEnabled =true
+                upiId.error ="Please Enter UPI ID"
+                returnValue=returnValue and false
+            }else if(!m.matches()){
+                upiId.isErrorEnabled =true
+                upiId.error ="Invalid UPI ID"
+                returnValue=returnValue and false
+            }else{
+                upiId.error =null
+                upiId.isErrorEnabled =false
+            }
+        }
+
+        if(upiPin!=null){
+            if(pin==""){
+                upiPin.isErrorEnabled =true
+                upiPin.error ="Please Enter UPI PIN"
+                returnValue=returnValue and false
+            }else if(pin.length==6||pin.length==4){
+                upiPin.error =null
+                upiPin.isErrorEnabled =false
+            }
+            else{
+                upiPin.isErrorEnabled =true
+                upiPin.error ="Invalid UPI PIN"
+                returnValue=returnValue and false
+            }
+        }
+
+        /*if(pin!=""&&id!=""){
+            if(m.matches() && (String.valueOf(pin).length == 6 || String.valueOf(pin).length == 4)){
+                return true
+            }
+        }*/
+        return returnValue
+        //return m.matches() && (String.valueOf(pin).length == 6 || String.valueOf(pin).length == 4)
+    }
+
+
+}
